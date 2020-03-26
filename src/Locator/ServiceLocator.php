abdd8448d0227ac49aaa7e7d6d050f2116abd999
Jascha van Aalst <mr.jascha@gmail.com>
2020-03-26 19:57:10 +0100

    Finished Locator

--- src/Locator/ServiceLocator.php
+++ src/Locator/ServiceLocator.php
@@ -2,7 +2,58 @@
 
 namespace Jascha030\WPOL\Service\Locator;
 
+use Exception;
+
 class ServiceLocator
 {
+    private $services = [];
 
+    /**
+     * ServiceContainer constructor.
+     *
+     * @param array $services
+     */
+    public function __construct($services = [])
+    {
+        foreach ($services as $service) {
+            if (class_exists($service)) {
+                $this[$service] = function () use ($service) {
+                    static $_service;
+
+                    if (null !== $_service) {
+                        return $_service;
+                    }
+
+                    $_service = (is_string($service)) ? new $service() : $service;
+
+                    return $_service;
+                };
+            }
+        }
+    }
+
+    /**
+     * @param $key
+     *
+     * @return bool|mixed
+     * @throws Exception
+     */
+    public function get($key)
+    {
+        if ($this->has($key)) {
+            throw new Exception("Service does not exist or is not loaded in plugin");
+        }
+
+        return call_user_func($this->services[$key]);
+    }
+
+    /**
+     * @param $key
+     *
+     * @return bool
+     */
+    public function has($key)
+    {
+        return (array_key_exists($key, $this->services));
+    }
 }
